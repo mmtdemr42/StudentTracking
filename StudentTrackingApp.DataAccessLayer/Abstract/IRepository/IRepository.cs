a631816762c0637fc120bca55b1b5a46737b2f05
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-09-22 10:28:53 +0300

    IRepository and GeneicRepository created.

---
+++ StudentTrackingApp.DataAccessLayer/Abstract/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTracking.DataAccessLayer.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        List<TEntity> List();
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        List<TEntity> List(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ StudentTrackingApp.DataAccessLayer/Concrete/Repository/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentTracking.DataAccessLayer.Abstract;
+using StudentTracking.DataAccessLayer.Concrete.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTracking.DataAccessLayer.Concrete.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        Context _context = new Context();
+        public void Delete(TEntity entity)
+        {
+            var deleteEntity = _context.Entry(entity);
+            deleteEntity.State = EntityState.Deleted;
+            _context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().SingleOrDefault(filter);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            var addedEntity = _context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            _context.SaveChanges();
+        }
+
+        public List<TEntity> List()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public List<TEntity> List(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            var updatedEntity = _context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
