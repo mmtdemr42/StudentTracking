4c1d43be798cc2db8ddd2d7453c1b4f8a70a5447
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-09-23 14:15:05 +0300

    MainTemplate and StudentLayout created.

---
+++ StudentTracking/Controllers/StudentDashboard.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentTracking.PresentationLayer.Controllers
+{
+    public class StudentDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
